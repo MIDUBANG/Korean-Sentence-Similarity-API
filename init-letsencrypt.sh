0a072b84ebd5ec9348dd0cff2fbbda2c42997674
ParkIsComing <maroony55@gmail.com>
2023-05-09 17:08:36 +0900

    fix : nginx 관련 문제 수정

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains="api.midubang.com"
 rsa_key_size=4096
-data_path="../volumes/certbot"
+data_path="./certbot"
 email="maroony55@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
