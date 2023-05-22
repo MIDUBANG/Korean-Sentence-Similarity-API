ab46f513a202fb71c1e470f2b62f5dd4ae4edbf0
ParkIsComing <maroony55@gmail.com>
2023-05-22 17:13:57 +0900

    feat : Celery 비동기 추가

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 echo "start docker-compose up: ubuntu"
-sudo docker-compose -f /home/ubuntu/srv/nlp_clova/docker-compose.yml up --build -d 
+sudo docker-compose -f /home/ubuntu/srv/nlp_clova/docker-compose.yml up --build -d
+cd /home/ubuntu/srv/nlp_clova/flask_app
+celery -A app.celery worker --loglevel=info
